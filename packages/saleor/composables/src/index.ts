4e8d977e2de8dbf99559ce5c9076d03882976b61
Ciprian Trusca <ciprian@sagital.ro>
2021-02-18 19:26:35 +0200

    Initial import, copied over the commercetools files

---
+++ packages/saleor/composables/src/index.ts
@@ -0,0 +1,34 @@
+/* istanbul ignore file */
+
+import useCategory from './useCategory';
+import useProduct from './useProduct';
+import useCart from './useCart';
+import useCheckout from './useCheckout';
+import useUser from './useUser';
+import useUserOrders from './useUserOrders';
+import { useReview } from './useReview';
+import useFacet from './useFacet';
+import useUserShipping from './useUserShipping';
+import useUserBilling from './useUserBilling';
+import useWishlist from './useWishlist';
+import useShipping from './useShipping';
+import { track } from '@vue-storefront/core';
+
+track('VSFCommercetools');
+
+export {
+  useCategory,
+  useProduct,
+  useCart,
+  useCheckout,
+  useUser,
+  useUserOrders,
+  useUserBilling,
+  useWishlist,
+  useUserShipping,
+  useReview,
+  useFacet,
+  useShipping
+};
+
+export * from './getters';
